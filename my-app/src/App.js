40af18023c273059f36ce25411b7b83a54f22dfc
marie-mccarthy <marie.mccarthy2000@gmail.com>
2021-11-17 16:23:42 -0700

    third commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 
 function Home() {
@@ -12,10 +13,27 @@ function Home() {
   function App() {
     return (
       <Router>
+            <nav style={{ margin: 10 }}>
+    <Link to="/" style={{ padding: 5 }}>
+      Home
+    </Link>
+    <Link to="/about" style={{ padding: 5 }}>
+      About
+    </Link>
+  </nav>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
+    );
+  }
+  function About() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h2>About View</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+      </div>
     );
   }
   export default App;
